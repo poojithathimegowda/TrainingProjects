0ae2f78db18ec979f430da19c1cbffa892177ad5
poojithathimegowda <poojithagowda98@gmail.com>
2024-07-02 21:39:20 +0200

    Added List Example

---
+++ PracticeProjects/ListExample.cs
@@ -0,0 +1,15 @@
+﻿public class ListExample
+{
+    static void ListExampleMain()
+    {
+        List<string> names = new List<string>()
+          {"Peter", "Michael", "George", "Victor", "John" };
+        names.Sort();
+        Console.WriteLine(string.Join(", ", names));
+        // George, John, Michael, Peter, Victor
+        names.Sort();
+        names.Reverse();
+        Console.WriteLine(string.Join(", ", names));
+        // Victor, Peter, Michael, John, George
+    }
+}
--- PracticeProjects/Program.cs
+++ PracticeProjects/Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main(string[] args)
     {
+        
     }
-    
+
 }
