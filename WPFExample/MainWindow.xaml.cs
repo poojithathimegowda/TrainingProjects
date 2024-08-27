229f9006e9a55f4c7cb6c4d494c85f42918214c0
poojithathimegowda <poojithagowda98@gmail.com>
2024-08-27 17:10:57 +0200

    Example for wpf

---
+++ WPFExample/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Boolean running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void RuntoggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                //stop
+                tbstatus.Text = "stopped";
+                RuntoggleButton.Content = "run";
+            }
+            else
+            {
+                //run
+                tbstatus.Text = "running";
+                RuntoggleButton.Content = "stop";
+            }
+           
+            running = !running;
+        }
+    }
+}
