ccca1b024c4309024fdc26c1203820d967db2470
poojithathimegowda <poojithagowda98@gmail.com>
2024-08-28 15:54:04 +0200

    added user control

--- WpfExample2/MainWindow.xaml.cs
+++ WpfExample2/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfExample2
+namespace WpfCustomControlDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
---
+++ WpfExample2/View/UserControl/MyCustomControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControlDemo
+{
+  
+
+    public partial class MyCustomControl : UserControl
+    {
+        public MyCustomControl()
+        {
+            InitializeComponent();
+        }
+
+        public string DisplayText
+        {
+            get { return (string)GetValue(DisplayTextProperty); }
+            set { SetValue(DisplayTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty DisplayTextProperty =
+            DependencyProperty.Register("DisplayText", typeof(string), typeof(MyCustomControl), new PropertyMetadata("Default Text"));
+    }
+
+
+
+}
